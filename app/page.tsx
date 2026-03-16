88879548ef5c581f5a909278489f7361a2314a75
TJ Boyle <thomas.john.boyle@gmail.com>
2026-03-16 15:22:00 -0400

    feat: get and show some singles from the API

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,66 @@
 import Image from "next/image";
 
-export default function Home() {
+export type Single = {
+  url: string
+  name: string
+  set_code: string
+  available_quantity: number
+  scryfall_id: string
+  price_cents: string
+  price_market: string
+}
+
+export default async function Home() {
+
+  // Map of scryfall_id to the image location
+  const scryfallIds: { [key: string]: string } = {
+    // On the Job
+    '48b92629-4196-4943-91fd-8c8d5f3fcaef': 'https://cards.scryfall.io/large/front/4/8/48b92629-4196-4943-91fd-8c8d5f3fcaef.jpg',
+
+    // Stamp of Approval
+    'e832a07d-b417-47fa-8a29-c31f590c2e59': 'https://cards.scryfall.io/large/front/e/8/e832a07d-b417-47fa-8a29-c31f590c2e59.jpg',
+
+    // Miracle Worker
+    '35d29bda-096c-44d4-b45e-c2c507f8efbe': 'https://cards.scryfall.io/large/front/3/5/35d29bda-096c-44d4-b45e-c2c507f8efbe.jpg'
+  }
+
+  const emailHeader = process.env.MANA_POOL_EMAIL || ''
+  const accessTokenHeader = process.env.MANA_POOL_ACCESS_TOKEN || ''
+
+  const data = await fetch(`https://manapool.com/api/v1/products/singles?scryfall_ids=${Object.keys(scryfallIds).join('&scryfall_ids=')}&languages=EN`, {
+    headers: {
+      'email': emailHeader,
+      'access-token': accessTokenHeader
+    }
+  })
+  const singles = await data.json().then((res) => res.data)
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="p-8">
+      <h1 className="text-3xl font-bold mb-6">Singles</h1>
+      {singles && singles.length > 0 ? (
+        <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-6">
+          {singles.map((single: Single) => (
+            <div key={single.name} className="border rounded-lg p-4 shadow-md hover:shadow-lg transition-shadow">
+              {single.scryfall_id && (
+                <Image
+                  src={scryfallIds[single.scryfall_id]}
+                  alt={single.name}
+                  width={640}
+                  height={891}
+                  className="rounded mb-2"
+                />
+              )}
+              <h2 className="text-xl font-semibold">{single.name}</h2>
+              <p className="text-gray-600 mb-2">{single.set_code}</p>
+              <p className="text-sm font-bold mb-1">Our Price: <span className="text-green-600">${single.price_cents}</span></p>
+              <p className="text-sm font-bold">Market Price: <span className="text-green-600">${single.price_market}</span></p>
+            </div>
+          ))}
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      ) : (
+        <p className="text-gray-500">No singles found</p>
+      )}
     </div>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      new URL('https://cards.scryfall.io/**'),
+      new URL('https://images.manapool.com/**')
+    ],
+  },
 };
 
 export default nextConfig;
